fcb9d2d34a0013950c88106baa4350c4732bc4d6
yuu <yuudj@hotmail.com>
2021-11-13 21:11:21 -0300

    enabing hash routes for github.io

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: 'vot1', loadChildren: () => import('./modules/vot1/vot1.module').then(m => m.Vot1Module) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
